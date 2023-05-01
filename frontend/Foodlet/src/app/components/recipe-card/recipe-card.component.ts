5842c75fbc5f328ff6295f10d15d996846b5079d
Rasmus Sandbæk <sandbxk@gmail.com>
2023-05-01 12:08:44 +0200

    init commit

---
+++ frontend/Foodlet/src/app/components/recipe-card/recipe-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-card',
+  templateUrl: './recipe-card.component.html',
+  styleUrls: ['./recipe-card.component.scss']
+})
+export class RecipeCardComponent {
+  title: any;
+
+}
