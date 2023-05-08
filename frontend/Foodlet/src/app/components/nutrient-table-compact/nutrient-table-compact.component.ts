e2cc13f83ecc3f84d44537b7add138c6e05185ba
Rasmus Sandbæk <sandbxk@gmail.com>
2023-05-08 19:00:35 +0200

    recipe card prototype done

--- frontend/Foodlet/src/app/components/nutrient-table-compact/nutrient-table-compact.component.ts
+++ frontend/Foodlet/src/app/components/nutrient-table-compact/nutrient-table-compact.component.ts
@@ -9,17 +9,38 @@ import {FoodItem} from "../../../domain/Recipe";
 export class NutrientTableCompactComponent {
   @Input() foodItems: FoodItem[] = []
 
-  nutrients: Map<string, number> = new Map<string, number>()
-  displayedColumns: string[] = ['name', 'amount'];
+  nutrients: any[] = []
 
+  nutrientsMap: Map<string, number> = new Map<string, number>()
+  displayedColumns: string[] = ['name', 'amount'];
+  private nutrientKeys: string[] = ['Protein', 'Carbohydrates', 'Fats', 'Saturated Fats', 'Fiber']
 
   ngOnInit(): void {
-    this.foodItems.forEach(
-     (value, index) => {
-       this.nutrients.set('Protein', value.protein)
+    this.nutrientKeys.forEach(
+      (value) => {
+        this.nutrientsMap.set(value, 0)
+      }
+    )
 
+    this.foodItems.forEach(
+     (value) => {
+       this.nutrientsMap.set('Protein', value.protein + this.nutrientsMap.get('Protein')!)
+       this.nutrientsMap.set('Carbohydrates', value.carbohydrates + this.nutrientsMap.get('Carbohydrates')!)
+       this.nutrientsMap.set('Fats', value.fat + this.nutrientsMap.get('Fats')!)
+       this.nutrientsMap.set('Saturated Fats', value.saturatedFat + this.nutrientsMap.get('Saturated Fats')!)
+       this.nutrientsMap.set('Fiber', this.nutrientsMap.get('Fiber')! + value.fiber)
      }
     )
+
+    this.nutrientKeys.forEach(
+      (value) => {
+        this.nutrients.push({
+          name: value,
+          amount: this.nutrientsMap.get(value)
+        })
+      }
+    )
+
   }
 
   constructor() { }
