18d548169d2286b55209f6baf649b42b1cca7181
Rasmus Sandbæk <sandbxk@gmail.com>
2023-05-08 18:19:54 +0200

    remove remaining nutrient.ts

--- frontend/Foodlet/src/app/components/nutrient-table-compact/nutrient-table-compact.component.ts
+++ frontend/Foodlet/src/app/components/nutrient-table-compact/nutrient-table-compact.component.ts
@@ -1,6 +1,5 @@
 import {Component, Input} from '@angular/core';
 import {FoodItem} from "../../../domain/Recipe";
-import {sumNutrients} from "../../../domain/converters";
 
 @Component({
   selector: 'app-nutrient-table-compact',
@@ -10,12 +9,17 @@ import {sumNutrients} from "../../../domain/converters";
 export class NutrientTableCompactComponent {
   @Input() foodItems: FoodItem[] = []
 
-  totalNutrients: Map<string, number> = new Map<string, number>()
+  nutrients: Map<string, number> = new Map<string, number>()
   displayedColumns: string[] = ['name', 'amount'];
 
 
   ngOnInit(): void {
+    this.foodItems.forEach(
+     (value, index) => {
+       this.nutrients.set('Protein', value.protein)
 
+     }
+    )
   }
 
   constructor() { }
