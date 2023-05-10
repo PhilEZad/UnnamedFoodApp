9053a20f38a5bf35264f7aae2e8304c007a797c4
Rasmus Sandbæk <sandbxk@gmail.com>
2023-05-10 10:49:52 +0200

    Update firebaseConfig.js

--- frontend/Foodlet/src/firebaseConfig.js
+++ frontend/Foodlet/src/firebaseConfig.js
@@ -6,7 +6,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyC4VwRsZVj0u6Pgsbcsbp6AoXIRI-YIfP4",
   authDomain: "foodlet-a2c4b.firebaseapp.com",
   projectId: "foodlet-a2c4b",
@@ -17,5 +17,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
