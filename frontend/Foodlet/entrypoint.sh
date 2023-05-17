587915ac6665ff2b42e7d94d1846544de968ae35
Mads Barth <mads-barth@live.dk>
2023-05-17 19:28:21 +0200

    .

--- frontend/Foodlet/entrypoint.sh
+++ frontend/Foodlet/entrypoint.sh
@@ -1,35 +1,50 @@
 #!/bin/bash
 
-
-echo $action
-echo "$action"
+echo "$firebase_token"
 
 if [ "$action" == "test" ]
 then
     npm run test
+    exit $?
 fi
 
 if [ "$action" == "lint" ]
 then
     npm run lint
+    exit $?
 fi
 
 if [ "$action" == "e2e" ]
 then
     npm run e2e
+    exit $?
 fi
 
 if [ "$action" == "build" ]
 then
     npm run build
+    exit $?
 fi
 
 if [ "$action" == "deploy" ]
 then
-    firebase deploy --token "$firebase_token"
+    firebase use "foodlet-a2c4b" --token "$firebase_token"
+
+    result=$(firebase deploy --non-interactive --token "$firebase_token" | grep "Error" | head -c1 | wc -c)
+    if [[ $result -ne 0 ]]; then
+      exit 1
+    fi
+    exit $?
+
 fi
 
 if [ "$action" == "firebase_check" ]
 then
-    firebase login:list --token "$firebase_token"
+    result=$(firebase use "foodlet-a2c4b" --token "$firebase_token" | grep "Error" | head -c1 | wc -c)
+    if [[ $result -ne 0 ]]; then
+      exit 1
+    fi
+    exit $?
 fi
+
+echo "invalid action environment variable [$action]"
