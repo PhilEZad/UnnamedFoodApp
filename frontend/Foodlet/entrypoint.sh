531d43136d3bade3e75d384974c5a45ceb24c8a8
Mads Barth <mads-barth@live.dk>
2023-05-17 20:01:15 +0200

    comments

--- frontend/Foodlet/entrypoint.sh
+++ frontend/Foodlet/entrypoint.sh
@@ -26,24 +26,29 @@ fi
 
 if [ "$action" == "deploy" ]
 then
+    # if firebase_check has passed we can assume it is safe to call here
     firebase use "foodlet-a2c4b" --token "$firebase_token"
 
+    # see firebase_check block
     result=$(firebase deploy --non-interactive --token "$firebase_token" | grep "Error" | head -c1 | wc -c)
     if [[ $result -ne 0 ]]; then
       exit 126
     fi
     exit $?
-
 fi
+
 
 if [ "$action" == "firebase_check" ]
 then
+    # grep      = find lines in output matching regex
+    # head -c1  = select first character in stdio
+    # wc -c     = true if any character else false
     result=$(firebase use "foodlet-a2c4b" --token "$firebase_token" | grep "Error" | head -c1 | wc -c)
-    if [[ $result -ne 0 ]]; then
-      exit 126
+    if [[ $result -ne 0 ]]; then # expect empty iostream
+      exit 126 # error is present (probably an invalid token)
     fi
-    exit $?
+    exit $? # reemit current system error code
 fi
 
 echo "invalid action environment variable [$action]"
-exit 127
+exit 127 # no program with that name (no valid action is present)
